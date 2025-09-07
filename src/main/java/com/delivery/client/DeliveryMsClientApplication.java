faa39b6a9bd29cfe33729f7db9002b89f372f963
Mateus Elias Vieira <mateus.elias.vieira@hotmail.com>
2025-09-07 11:09:25 -0300

    first commit in branch develop

---
+++ src/main/java/com/delivery/client/DeliveryMsClientApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryMsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivery/client/DeliveryMsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
